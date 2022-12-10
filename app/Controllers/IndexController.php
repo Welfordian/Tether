ce81bc502eb294ece28a86fd9cb3fa0e8322af2b
Joshua Welford <Josh@fish.cat>
2022-12-10 14:25:30 -0500

    WIP

--- app/Controllers/IndexController.php
+++ app/Controllers/IndexController.php
@@ -2,23 +2,10 @@
 
 namespace App\Controllers;
 
-use App\Models\User;
-use App\Models\YoutubeVideo;
-
 class IndexController extends Controller
 {
     public function show(): string
-    {
-        $users = User::all();
-        $videos = (new YoutubeVideo())->select([])->get();
-        
-        echo "<pre>";
-        print_r($users);
-        print_r($videos);
-        echo "</pre>";
-        
-        die();
-        
+    {        
         return $this->view('index');
     }
 }
